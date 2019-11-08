dbe76eae80b305a015e31b56f8823dc20c9e50cb
AndreaMendi <andreamendi@icloud.com>
2019-11-07 22:04:54 -0600

    Se implementaron las acciones de Mongo DB

--- movies-api/lib/mongo.js
+++ movies-api/lib/mongo.js
@@ -1,5 +1,5 @@
-const { MongoCLient, ObjectId } = require('mongodb');
-const { config } = require('../config');
+const { MongoCLient, ObjectId } = require("mongodb");
+const { config } = require("../config");
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
@@ -8,46 +8,67 @@ const DB_NAME = config.dbName;
 const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
 
 class MongoLib {
-    constructor() {
-        this.client = new MongoCLient(MONGO_URI, { useNewUrlParser: true });
-        this.dbName = DB_NAME;
+  constructor() {
+    this.client = new MongoCLient(MONGO_URI, { useNewUrlParser: true });
+    this.dbName = DB_NAME;
+  }
+
+  connect() {
+    if (!MongoLib.connection) {
+      MongoLib.connection = new Promise((resolve, reject) => {
+        this.client.connect(err => {
+          if (err) {
+            reject(err);
+          }
+          console.log("Connected succesfully to Mongo");
+          resolve(this.client.db(this.dbName));
+        });
+      });
     }
 
-    connect() {
-        if(!MongoLib.connection) {
-            MongoLib.connection = new Promise((resolve, reject) => {
-                this.client.connect(err => {
-                    if(err){
-                        reject(err);
-                    } 
-                    console.log('Connected succesfully to Mongo');
-                    resolve(this.client.db(this.dbName));
-                })
-            })
-        }
+    return MongoLib.connection;
+  }
 
-        return MongoLib.connection;
-    }
+  getAll(collection, query) {
+    return this.connect().then(db => {
+      return db
+        .collection(collection)
+        .find(query)
+        .toArray();
+    });
+  }
 
-    getAll() {
+  get(collection, id) {
+    return this.connect().then(db => {
+      return db.collection(collection).findOne({ _id: ObjectId(id) });
+    });
+  }
 
-    }
+  create(collection, data) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).insertOne(data);
+      })
+      .then(result => result.insertedId);
+  }
 
-    get (){
+  update(collection, id, data) {
+    return this.connect()
+      .then(db => {
+        return db
+          .collection(collection)
+          .updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
+      })
+      .then(result => result.upsertedId || id);
+  }
 
-    }
-
-    create() {
-
-    }
-
-    update(){
-
-    }
-
-    delete() {
-
-    }
+  delete(collection, id) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).deleteOne({ _id: ObjectId(id) });
+      })
+      .then(() => id);
+  }
 }
 
-module.exports  = MongoLib;
+module.exports = MongoLib;
